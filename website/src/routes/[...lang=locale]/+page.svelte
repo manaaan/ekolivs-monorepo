3466e29bfe73be3f5cf22bdbc6bb7366f1b6d8dd
kappl <jakob+git@kappl.dk>
2023-12-02 11:05:29 +0100

    feat: Add content to frontpage, including sloppy styling

--- website/src/routes/[...lang=locale]/+page.svelte
+++ website/src/routes/[...lang=locale]/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { List, Li } from 'flowbite-svelte';
+  import { CheckOutline } from 'flowbite-svelte-icons';
   import { t } from '$lib/translations';
 </script>
 
@@ -8,4 +10,27 @@
   <meta name="robots" content="index, follow" />
 </svelte-head>
 
-<p class="text-gray-800 dark:text-gray-200">{$t('home.text-1')}</p>
+<div class="grid grid-cols-1 md:grid-cols-2 gap-8 py-8">
+  <div class="max-w-xl ml-auto">
+    <h1 class="text-3xl font-extrabold mb-4">
+      {$t('home.section.intro.header')}
+    </h1>
+    <p>
+      {$t('home.section.intro.p-1')}
+    </p>
+  </div>
+  <List tag="ul" class="max-w-xl ml-auto space-y-3" list="none">
+    <Li icon class="gap-3">
+      <CheckOutline class="w-3.5 h-3.5 text-green-500 dark:text-green-400" />
+      {$t('home.section.principles.principle-1')}
+    </Li>
+    <Li icon class="gap-3">
+      <CheckOutline class="w-3.5 h-3.5 text-green-500 dark:text-green-400" />
+      {$t('home.section.principles.principle-2')}
+    </Li>
+    <Li icon class="gap-3">
+      <CheckOutline class="w-3.5 h-3.5 text-green-500 dark:text-green-400" />
+      {$t('home.section.principles.principle-3')}
+    </Li>
+  </List>
+</div>
